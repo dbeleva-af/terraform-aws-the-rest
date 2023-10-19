2133d175bbead330fd2a09e21fb9a8cb5f4abd24
dbeleva-af <dbeleva_af@abv.bg>
2023-10-19 13:07:23 +0300

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,33 @@
+resource "random_pet" "Dog" {
+  length = var.length
+}
+
+variable "length" {
+ type = number
+ default = "9"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Hello"
+  }
+}
